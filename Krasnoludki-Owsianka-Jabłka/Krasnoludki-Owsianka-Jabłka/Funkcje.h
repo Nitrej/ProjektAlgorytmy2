b3ff44fa3c5d23e3b41079059e3c000f68644fe9
RecoilCoding <kacperzsmeie@gmail.com>
2023-07-05 21:57:44 +0200

    +funkcje przyporzadkujKrasnaleMaksWydobycie oraz przyporzadkujKrasnaleMaksWydobycieMaloOwsianki

--- Krasnoludki-Owsianka-Jabłka/Krasnoludki-Owsianka-Jabłka/Funkcje.h
+++ Krasnoludki-Owsianka-Jabłka/Krasnoludki-Owsianka-Jabłka/Funkcje.h
@@ -66,3 +66,84 @@ int obliczOdleglosc(int kopalniaX, int kopalniaY, int krasnoludekX, int krasnolu
     int dy = abs(kopalniaY - krasnoludekY);
     return sqrt(dx * dx + dy * dy);
 }
+
+//Maksymalne wydobycie
+void przyporzadkujKrasnaleMaksWydobycie(vector<Krasnoludek>& dwarves, vector<Kopalnia>& mines) {
+    for (Krasnoludek& dwarf : dwarves) {
+        int best_mine = -1;
+        int best_efficiency = -1;
+
+        for (size_t i = 0; i < mines.size(); i++) {
+            Kopalnia& mine = mines[i];
+
+            if (mine.wydobywanySurowiec == dwarf.preferowanyFach && mine.indeksyPracujacychKrasnali.size() < mine.limitMiejsc) {
+                if (best_mine == -1 || dwarf.wydajnosc > best_efficiency) {
+                    best_mine = i;
+                    best_efficiency = dwarf.wydajnosc;
+                }
+            }
+        }
+
+        if (best_mine == -1) {
+            // Przypisanie do kopalni z wolnym miejscem
+            for (size_t i = 0; i < mines.size(); i++) {
+                Kopalnia& mine = mines[i];
+                if (mine.indeksyPracujacychKrasnali.size() < mine.limitMiejsc) {
+                    best_mine = i;
+                    break;
+                }
+            }
+        }
+
+        if (best_mine != -1) {
+            mines[best_mine].indeksyPracujacychKrasnali.push_back(&dwarf - &dwarves[0]);
+        }
+    }
+}
+
+//Maksymalne wydobycie z minimalnymi odleglosciami od kopalnii
+void przyporzadkujKrasnaleMaksWydobycieMaloOwsianki(vector<Krasnoludek>& dwarves, vector<Kopalnia>& mines) {
+    for (Krasnoludek& dwarf : dwarves) {
+        int best_mine = -1;
+        int best_efficiency = -1;
+        int best_distance = numeric_limits<int>::max();
+
+        for (size_t i = 0; i < mines.size(); i++) {
+            Kopalnia& mine = mines[i];
+
+            if (mine.wydobywanySurowiec == dwarf.preferowanyFach && mine.indeksyPracujacychKrasnali.size() < mine.limitMiejsc) {
+                int distance = obliczOdleglosc(dwarf.polozenieX, dwarf.polozenieY, mine.polozenieX, mine.polozenieY);
+
+                if (best_mine == -1 || (dwarf.wydajnosc > best_efficiency) ||
+                    (dwarf.wydajnosc == best_efficiency && distance < best_distance)) {
+                    best_mine = i;
+                    best_efficiency = dwarf.wydajnosc;
+                    best_distance = distance;
+                }
+            }
+        }
+
+        if (best_mine == -1) {
+            // Przypisanie kopalni z najbli¿szym preferowanym surowcem
+            int closest_mine = -1;
+            int closest_distance = numeric_limits<int>::max();
+
+            for (size_t i = 0; i < mines.size(); i++) {
+                Kopalnia& mine = mines[i];
+                if (mine.indeksyPracujacychKrasnali.size() < mine.limitMiejsc) {
+                    int distance = obliczOdleglosc(dwarf.polozenieX, dwarf.polozenieY, mine.polozenieX, mine.polozenieY);
+                    if (distance < closest_distance) {
+                        closest_mine = i;
+                        closest_distance = distance;
+                    }
+                }
+            }
+
+            best_mine = closest_mine;
+        }
+
+        if (best_mine != -1) {
+            mines[best_mine].indeksyPracujacychKrasnali.push_back(&dwarf - &dwarves[0]);
+        }
+    }
+}
