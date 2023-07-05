77ebb5b9e30d628e3207860de9ed355205feb18a
korsem <korneliasem@gmail.com>
2023-07-06 00:53:45 +0200

    testy #0,5

--- tests/main_testy.cpp
+++ tests/main_testy.cpp
@@ -1,10 +1,11 @@
 #include "gtest/gtest.h"
 #include "obliczOdlegloscTest.h"
+#include "compareDwarfsTest.h"
+#include "przyporzadkujKrasnaleMaksWydobycieTest.h"
+#include "przyporzadkujKrasnaleMaksWydobycieMaloOwsiankiTest.h"
 
-//Testy jednostkowe dla funkcji compareDwarfs,
-//przyporzadkujKrasnaleMaksWydobycie,
-//przyporzadkujKrasnaleMaksWydobycieMaloOwsianki
 
+// przy takim ustawieniu sie nie odpala wszytskie
 int test_main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
