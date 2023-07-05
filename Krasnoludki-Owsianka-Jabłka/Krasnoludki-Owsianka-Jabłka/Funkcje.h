66ca98fb74ad7fc664a01d0f905e70b59e5efa78
RecoilCoding <kacperzsmeie@gmail.com>
2023-07-05 21:12:19 +0200

    +porownanie krasnoludkow

--- Krasnoludki-Owsianka-Jabłka/Krasnoludki-Owsianka-Jabłka/Funkcje.h
+++ Krasnoludki-Owsianka-Jabłka/Krasnoludki-Owsianka-Jabłka/Funkcje.h
@@ -1,7 +1,13 @@
 #pragma once
 #include <fstream>
+#include "krasnoludek.h"
 
 bool wczytanieDanych() {
 
 	return false;
+}
+
+// Funkcja porównuj¹ca dla sortowania
+bool compareDwarfs(const Krasnoludek& krasnoludek1, const Krasnoludek& krasnoludek2) {
+	return krasnoludek1.wydajnosc > krasnoludek2.wydajnosc;
 }
